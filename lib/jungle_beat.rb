f3aa71ca97cb218cbc67883598abc0099842d0a7
kylesallette <kylesallette@gmail.com>
2017-12-05 13:19:25 -0700

    added voice and speed control

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -3,14 +3,27 @@ require_relative 'linked_list'
 
 class JungleBeat
 
+  attr_accessor :rate,
+                :voice
+
   def initialize
     @list = LinkedList.new
+    @rate = 500
+    @voice = "Boing"
   end
 
   def play
      beats = @list.to_string
-    `say -r 500 -v Boing #{beats}`
+    `say -r #{rate} -v #{voice} #{beats}`
   end
+
+  def reset_rate
+    @rate = 500
+  end
+
+  def reset_voice
+    @voice = "Boing"
+  end 
 
   def head
     @head
